45f056553fa617e0d29e0d2fb0be342d27ca5aae
cherylmoestadja <122387672+cherylmoestadja@users.noreply.github.com>
2023-02-04 16:04:56 +0100

    add updated conditions file

--- conditions/main.py
+++ conditions/main.py
@@ -12,8 +12,8 @@ __human_name__ = 'conditions'
 def farm_action(weather, time_of_day, cow_milking_status, location_cows, season, slurry_tank, grass_status_long):
     if time_of_day == 'night' and location_cows == 'pasture':
         return('take cows to cowshed')
-    elif weather == 'rainy':
-        return('take cows to cowshed')
+        if weather == 'rainy':
+            return('take cows to cowshed')
     elif cow_milking_status == True and location_cows == 'cowshed': 
         return('milk cows')
     elif slurry_tank == True and location_cows == 'cowshed' and weather != 'sunny'and weather != 'windy':
@@ -21,11 +21,13 @@ def farm_action(weather, time_of_day, cow_milking_status, location_cows, season,
     elif grass_status_long == True and season == 'spring' and weather == 'sunny' and location_cows != 'pasture':
         return('mow grass')
     if weather == 'sunny' and time_of_day == 'day' and cow_milking_status == True and location_cows == 'pasture' and season == 'spring' and slurry_tank == False and grass_status_long == True:
-        return ('take cows to cowshed \nmilk cows \ntake cows back to pasture')
+            return ('take cows to cowshed\nmilk cows\ntake cows back to pasture')
     else:
         return('wait')
 
+
 print(farm_action('rainy', 'night', True, 'pasture', 'spring', True, True))
 print(farm_action('sunny', 'day', True, 'pasture', 'spring', False, True))
+
 
 
